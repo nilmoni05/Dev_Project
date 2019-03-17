fbd704e136ca4129e7326ac701fe746d7e4f37d9
nilmoni05 <nilmoni.chakrabarty05@gmail.com>
2019-03-17 20:01:34 +0530

    Update MyClass.java
    Second developer changes second time

--- My_Project/MyClass.java
+++ My_Project/MyClass.java
@@ -3,5 +3,6 @@ public class MyClass{
 	public static void main(String[] args){
 			System.out.println("This is my first project");
 			System.out.println("This is first developer");
+			System.out.println("This is second developer");
 	}
 }
